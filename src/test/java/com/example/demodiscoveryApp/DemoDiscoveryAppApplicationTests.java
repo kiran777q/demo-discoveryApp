a2e97981594462b5d3db34e571f10273b4a8cd64
Naga Kiran Kumar Yanike <naga.kiran.kumar.yanike@ericsson.com>
2024-02-24 15:51:06 -0500

    Initial commit

---
+++ src/test/java/com/example/demodiscoveryApp/DemoDiscoveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodiscoveryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDiscoveryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
